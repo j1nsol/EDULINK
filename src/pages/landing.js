387660ff5102eab92083c5a07ce1120d610daa9a
Wl-lO <flextervega@gmail.com>
2024-07-26 05:42:41 +0800

    profile-changes

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -8,13 +8,13 @@ import './landing.css';
 import marbench from "../images/marbench (1).jpg";
 import flex from "../images/flex.jpg";
 import dpsample from "../images/dpsample.png";
-import tbd from "../images/tbd.png";
+import jim from "../images/jim.png";
 
 const developers = [
   { name: 'Carry Ana', imgSrc: dpsample, description: 'ako ang lider sa mga wakwak ,wa koy tog di sa sig code pero tungod sa ako alter ego nga si god Anna' },
   { name: 'Support is layp', imgSrc: flex, description: 'batos batos sa lider sa wak2, wa koy tog pero wa pd ko gacode, moral support lng jd ko aring dapita' },
   { name: 'idol nakos Ana', imgSrc: marbench, description: 'idol nako si ana mao ng akong programming langguage gigamit kay bisaya developed by jimmy neutron' },
-  { name: 'pos 1 ng naspi pero pos3 sa ko ron', imgSrc: tbd, description: 'akoy bahala sa muunag lihok ug himo pero sila nay mutiwas, kay matod pa sa hulagway "the early bird wakes up early"-support is layp' }
+  { name: 'pos 1 ng naspi pero pos3 sa ko ron', imgSrc: jim, description: 'akoy bahala sa muunag lihok ug himo pero sila nay mutiwas, kay matod pa sa hulagway "the early bird wakes up early"-support is layp' }
 ];
 
 function Landing() {
